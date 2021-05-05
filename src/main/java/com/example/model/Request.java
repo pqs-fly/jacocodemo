a104427f3d8fb82d9493a2c112b453d7bf6ed21c
peiqiangsheng <peiqiangsheng@100tal.com>
2021-05-05 18:43:25 +0800

    aa

---
+++ src/main/java/com/example/model/Request.java
@@ -0,0 +1,13 @@
+package com.example.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class Request {
+    MathOperate mathOperate;
+    int firstNum;
+    int secondNum;
+}
---
+++ src/test/java/com/example/service/MathServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+import org.junit.Test;
+
+public class MathServiceTest {
+    @Test
+    public void addTest()
+    {
+        MathService.add(1,1);
+    }
+}
